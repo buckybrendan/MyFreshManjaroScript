e856ebb03e49f74bc92d5be166bc10a6ee1218d8
buckybrendan <77645273+buckybrendan@users.noreply.github.com>
2022-07-26 02:57:32 +0100

    Update FreshManjaro.sh

--- FreshManjaro.sh
+++ FreshManjaro.sh
@@ -1,26 +1,23 @@
-sudo pacman -Rcns manjaro-hello manjaro-welcome
-sudo pacman-mirrors --fasttrack
-sudo pacman -Syyu
+sudo pacman -Rnsc manjaro-hello manjaro-welcome
+pacman-key --init
+pacman-key --populate archlinux manjaro
+cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
+sudo pacman-mirrors --fasttrack 15 --api --set-branch unstable --protocol https && sudo pacman -Syyuu
 sudo sed --in-place "s/#EnableAUR/EnableAUR/" "/etc/pamac.conf
 sudo sed --in-place "s/#CheckAURUpdates/CheckAURUpdates/" "/etc/pamac.conf"
-sudo pacman -S piper thunderbird gwe keepassxc steam-native steam-manjaro htop lutris gamemode heroic-games-launcher-bin pamac
+sudo pacman -S piper thunderbird gwe keepassxc steam-manjaro htop lutris mangohud lib32-mangohud mangohud-common gamemode lib32-gamemode gamescope wine-staging winetricks giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo libxcomposite lib32-libxcomposite libxinerama lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader cups samba dosbox caprine 
 sudo nvidia-xconfig --cool-bits=24
-sudo pacman -S wine-staging winetricks
-sudo pacman -S giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo libxcomposite lib32-libxcomposite libxinerama lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader cups samba dosbox
-sudo pamac build protontricks-git proton-ge-custom-bin discord_arch_electron legendary rare cointop lutris-battlenet-meta
+sudo pamac build protontricks-git discord_arch_electron runescape-launcher bottles goverlay-bin 
 cat /proc/sys/vm/swappiness
 sudo echo "vm.swappiness=20" > /etc/sysctl.d/100-manjaro.conf
-sudo systemctl enable fstrim.timer
-sudo systemctl start fstrim.timer
 curl -O https://gitlab.com/cscs/maxperfwiz/raw/master/maxperfwiz
 chmod +x maxperfwiz
 mhwd-kernel -l
 mhwd-kernel -li
-sudo pacman -Syyu
-sudo sudo pacman -Syu grub
+sudo pacman -Syyuu
+#sudo pacman -Syu grub
 sudo systemctl --failed
 sudo journalctl -p 3 -xb
-
-##Brendan Mounting NAS##
-sudo mkdir /mnt/NAS
-sudo -- bash -c 'echo "//192.168.1.LASTID/brendan /mnt/NAS cifs uid=1001,username=brendan,password=PASWORD,domain=WORKGROUP,nofail,noauto,x-systemd.automount,x-systemd.mount-timeout=30,_netdev 0 0" >> etc/fstab'
+pacman -Sc
+sudo systemctl enable fstrim.timer
+sudo systemctl start fstrim.timer
